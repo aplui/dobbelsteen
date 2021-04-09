b232b5d8ce84e22b32f82810eceac66ef4ea5247
aplui <alexander.pluimers@gmail.com>
2021-04-09 21:46:29 +0200

    working

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json")
 
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
   
   client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
+    function randomNumber(min,max){
+      return Math.floor(Math.random()*(max-min+1)+min);
+  }
+
+    if (msg.content === '!roll') {
+      msg.reply(randomNumber(1,100));
     }
   });
   
