53cdcf8d5176a516560d1da41ab1cc408d7e46ee
aplui <alexander.pluimers@gmail.com>
2021-04-11 17:01:36 +0200

    fix yaml

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const config = require("./config.json")
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("!roll to roll"); 
   });
   
   client.on('message', msg => {
     function randomNumber(min,max){
       return Math.floor(Math.random()*(max-min+1)+min);
   }
-
     if (msg.content === '!roll') {
       msg.reply(randomNumber(1,100));
     }
